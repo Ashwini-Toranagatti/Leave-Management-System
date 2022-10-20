9798cf51c169e2030d5659b369f9cdc564801dae
Ashwini.T <97398492+Ashwini-Toranagatti@users.noreply.github.com>
2022-10-20 09:40:19 +0530

    Add files via upload

---
+++ dbconnection.php
@@ -0,0 +1,19 @@
+<?php
+//db connection
+    $servername = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "lms";
+
+// Create connection
+	$conn = mysqli_connect($servername, $username, $password,$db);
+
+	// Check connection
+	if (!$conn) {
+	  die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	
+	
+	echo "";
+?>
